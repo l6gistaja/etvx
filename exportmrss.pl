a1b161c1fdb82624013f5eee869d644e571e4349
Juks <juks@alkohol.ee>
2012-04-08 13:44:20 +0300

    MRSS fixes

---
+++ exportmrss.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use POSIX qw/strftime/;
+use DBI;
+# you also need DBD:SQLite;
+$rfc822 = '%a, %d %b %Y %H:%M:%S %Z';
+
+print '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+print '<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">'."\n";
+print '<channel>'."\n";
+print '<title>ETVx MRSS export</title>'."\n";
+print '<pubDate>'.strftime($rfc822,localtime).'</pubDate>'."\n";
+
+my $dbh = DBI->connect("dbi:SQLite:development.sqlite3", "", "",
+{RaiseError => 1, AutoCommit => 1});
+my $sth = $dbh->prepare("select b.filename, b.name, b.description, c.name as cname, t1ts-t0ts as duration, c.id as cid, b.t0ts, b.id from broadcasts b, channels c where (b.filename is not null or b.filename <> '') and b.channel_id=c.id");
+$sth->execute();
+while(my $result = $sth->fetchrow_hashref()) {
+    print '<item>'."\n";
+    print '<title>'.$result->{'name'}.'</title>'."\n";
+    if($result->{'description'} ne '') {
+        print '<description>'.$result->{'description'}.'</description>'."\n";
+    }
+    print '<pubDate>'.strftime($rfc822,gmtime($result->{'t0ts'})).'</pubDate>'."\n";
+    print '<media:content url="'.$result->{'filename'}.'" duration="'.$result->{'duration'}.'" medium="video"/>'."\n";
+    print '<guid>jae/etvx/c='.sprintf("%x",$result->{'cid'})
+        .'/t='.sprintf("%x",$result->{'t0ts'}/60)
+        .'/i='.sprintf("%x",$result->{'id'}).'</guid>'."\n";
+    #print '<media:keywords>'.$result->{'cname'}.'</media:keywords>'."\n";
+    print '</item>'."\n";
+}
+
+$sth->finish();
+$dbh->disconnect;
+print '</channel>'."\n";
+print '</rss>'."\n";
+
