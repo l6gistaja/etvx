9be5995f3ee79cc61f564eb33a9e42cfc794ec14
Juks <juks@alkohol.ee>
2012-05-03 10:27:57 +0300

    exporting all broadcasts added

--- exportmrss.pl
+++ exportmrss.pl
@@ -2,7 +2,15 @@
 use POSIX qw/strftime/;
 use DBI;
 # you also need DBD:SQLite;
+use Getopt::Std;
 $rfc822 = '%a, %d %b %Y %H:%M:%S %Z';
+
+use vars qw/ %opt /;
+getopts( 'ha', \%opt );
+if ($opt{h}) {
+    print "\nUsage:\n-a key exports all broadcasts. By default only downloaded broadcasts are exported.\n\n";
+    exit;
+}
 
 print '<?xml version="1.0" encoding="UTF-8"?>'."\n";
 print '<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">'."\n";
@@ -12,7 +20,9 @@ print '<pubDate>'.strftime($rfc822,localtime).'</pubDate>'."\n";
 
 my $dbh = DBI->connect("dbi:SQLite:development.sqlite3", "", "",
 {RaiseError => 1, AutoCommit => 1});
-my $sth = $dbh->prepare("select b.filename, b.name, b.description, c.name as cname, t1ts-t0ts as duration, c.id as cid, b.t0ts, b.id from broadcasts b, channels c where (b.filename is not null or b.filename <> '') and b.channel_id=c.id");
+my $sth = $dbh->prepare("select b.filename, b.name, b.description, c.name as cname, t1ts-t0ts as duration, c.id as cid, b.t0ts, b.id from broadcasts b, channels c where "
+    .($opt{a} ? "" : "(b.filename is not null or b.filename <> '') and ")
+    ."b.channel_id=c.id");
 $sth->execute();
 while(my $result = $sth->fetchrow_hashref()) {
     print '<item>'."\n";
